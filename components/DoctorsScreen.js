4d474fdb6b2dbcc4f1b8f41801bedf35def3d883
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-22 12:38:49 +0000

    added: Doctors component

---
+++ components/DoctorsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const DoctorsScreen = () => (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Doctors Screen</Text>
+    </View>
+);
+
+export default DoctorsScreen;
