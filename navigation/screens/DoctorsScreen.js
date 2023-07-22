8c9083fceccfc886ec1330fb3bdb873327c91e8d
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-22 17:42:05 +0000

    modified: DoctorsScreen

--- navigation/screens/DoctorsScreen.js
+++ navigation/screens/DoctorsScreen.js
@@ -1,10 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
+import DoctorCard from './DoctorCard';
 
-const DoctorsScreen = () => (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Doctors Screen</Text>
-    </View>
-);
+const doctorsData = [
+    {
+        id: '1',
+        name: 'Dr. Daniel Kissiedu',
+        position: 'Cardiologist',
+        profilePicture: require('../../assets/doctor1.png'),
+    },
+    {
+        id: '2',
+        name: 'Dr. Felix Ashong',
+        position: 'Pediatrician',
+        profilePicture: require('../../assets/doctor2.jpeg'),
+    },
+    {
+        id: '3',
+        name: 'Dr. Samuel Agyei',
+        position: 'Dermatologist',
+        profilePicture: require('../../assets/doctor3.jpeg'),
+    },
+];
+
+const DoctorsScreen = () => {
+    const renderDoctorCard = ({item}) => (
+        <DoctorCard
+            name={item.name}
+            position={item.position}
+            profilePicture={item.profilePicture}
+        />
+    );
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={doctorsData}
+                renderItem={renderDoctorCard}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#F5F5F5',
+    },
+});
 
 export default DoctorsScreen;
