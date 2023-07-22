40eeea7f26df7010eb206dc9462aad9b284ca6ee
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-22 17:57:37 +0000

    modified: HomeScreen Activity

---
+++ navigation/screens/DoctorCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const DoctorCard = ({name, position, profilePicture}) => {
+    return (
+        <View style={styles.container}>
+            <Image source={profilePicture} style={styles.profilePicture} />
+            <View style={styles.textContainer}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.position}>{position}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        marginBottom: 10,
+        padding: 16,
+    },
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+    },
+    textContainer: {
+        flex: 1,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    position: {
+        fontSize: 16,
+        color: '#555',
+    },
+});
+
+export default DoctorCard;
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,9 +1,75 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
 
-const HomeScreen = () => (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Home Screen</Text>
-    </View>
-);
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('../../assets/healthbg.png')}
+                style={styles.backgroundImage}
+                resizeMode='cover'
+            >
+                <View style={styles.contentContainer}>
+                    <Text style={styles.hospitalName}>Welcome to Clinics</Text>
+                    <Text style={styles.achievementTitle}>Achievements</Text>
+                    <Text style={styles.achievementText}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nulla eu elit et diam aliquet consectetur.
+                    </Text>
+                    <Text style={styles.goalTitle}>Goals</Text>
+                    <Text style={styles.goalText}>
+                        Suspendisse potenti. Proin lacinia metus sed sapien
+                        gravida, vel facilisis purus facilisis.
+                    </Text>
+                </View>
+            </ImageBackground>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    contentContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.7)', // Add a semi-transparent black overlay on the background image
+        padding: 20,
+        borderRadius: 10,
+        maxWidth: 350,
+    },
+    hospitalName: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    achievementTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 8,
+    },
+    achievementText: {
+        fontSize: 16,
+        color: '#FFF',
+        marginBottom: 16,
+    },
+    goalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginBottom: 8,
+    },
+    goalText: {
+        fontSize: 16,
+        color: '#FFF',
+    },
+});
+
 export default HomeScreen;
