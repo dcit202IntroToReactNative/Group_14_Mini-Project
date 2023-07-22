c3a2dc590111ef786890f3661a806a92ec31c433
ayyesu <iamdanielkissiedu@gmail.com>
2023-07-22 14:02:03 +0000

    files modified

--- components/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-
-const HomeScreen = () => (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Home Screen</Text>
-    </View>
-);
-export default HomeScreen;
